adc4e00dfc8e7f43dcffa1840b21019bb40c1f94
iloveteajay <atytjani@gmail.com>
2022-10-06 19:02:05 +0100

    Initial Commit

---
+++ coffeemasters/coffeemastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeemastersApp.swift
+//  coffeemasters
+//
+//  Created by Yusuf Ahmed on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct coffeemastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
