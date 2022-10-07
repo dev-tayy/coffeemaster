e7a6a9a285f01ace0781af3f99af901a95b50d67
iloveteajay <atytjani@gmail.com>
2022-10-07 08:25:54 +0100

    initial commit

--- coffeemasters/ContentView.swift
+++ coffeemasters/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Hello, world!")
+                .padding()
+        }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
